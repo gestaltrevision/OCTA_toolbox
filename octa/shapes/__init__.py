5843895373958fbce26feb01741d93368e565efd
ChristopheBossens <christophe.bossens@gmail.com>
2020-05-06 11:54:35 +0200

    Added Curve class

--- octa/shapes/__init__.py
+++ octa/shapes/__init__.py
@@ -9,3 +9,4 @@ Created on Tue May  5 15:58:04 2020
 from octa.shapes.Rectangle import Rectangle
 from octa.shapes.Ellipse import Ellipse
 from octa.shapes.Triangle import Triangle
+from octa.shapes.Curve import Curve
--- octa_toolbox_demo.py
+++ octa_toolbox_demo.py
@@ -10,8 +10,8 @@ from octa.Stimulus import Stimulus
 from octa.Positions import Positions
 from octa import shapes, patterns
 
-n_rows = 10
-n_cols = 10
+n_rows = 5
+n_cols = 5
 
 row_spacing = 50
 col_spacing = 50
@@ -19,16 +19,16 @@ col_spacing = 50
 stimulus = Stimulus(background_color = "white")
 
 stimulus.positions   = Positions.Create2DGrid(n_rows, n_cols, row_spacing, col_spacing)
-stimulus.shapes      = patterns.BasicPattern([shapes.Triangle, shapes.Rectangle]).DuplicatePatternToSize(n_rows * n_cols)
-stimulus.size        = patterns.BasicPattern([20, (10, 40)]).DuplicatePatternToSize(n_rows * n_cols)
+stimulus.shapes      = patterns.BasicPattern([shapes.Curve, shapes.Rectangle]).DuplicatePatternToSize(n_rows * n_cols)
+stimulus.size        = patterns.BasicPattern([30]).DuplicatePatternToSize(n_rows * n_cols)
 stimulus.colour      = patterns.GridGradient("red", "green", n_rows, n_cols).GradientAcrossRightDiagonal()
-stimulus.orientation = patterns.SymmetryPattern([0,12,45], n_rows, n_cols).MirrorAcrossColumns()
+stimulus.orientation = patterns.SymmetryPattern([0], n_rows, n_cols).MirrorAcrossColumns()
 stimulus.Render()
 stimulus.Show()
-stimulus.SaveJSON(r'output/test_stimulus')
-stimulus.SaveSVG(r'output/test_stimulus')
+#stimulus.SaveJSON(r'output/test_stimulus')
+#stimulus.SaveSVG(r'output/test_stimulus')
 
 #%%
-stim_2 = Stimulus.LoadFromJSON(r"output/test_stimulus.json")
-stim_2.Render()
-stim_2.Show()
+# stim_2 = Stimulus.LoadFromJSON(r"output/test_stimulus.json")
+# stim_2.Render()
+# stim_2.Show()
