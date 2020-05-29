e5441549ca5cb8fe0f693abaf40fb734d5f0433f
ChristopheBossens <christophe.bossens@gmail.com>
2020-05-29 15:37:16 +0200

    Cleanup after update

--- octa/patterns/__init__.py
+++ octa/patterns/__init__.py
@@ -8,6 +8,3 @@ Created on Tue May  5 15:58:04 2020
 from octa.patterns.Pattern import Pattern
 
 import octa.patterns.GridPattern
-#from octa.patterns.GradientPattern import GridGradient
-#from octa.patterns.RepeaterPattern import GridRepeater
-#from octa.patterns.SymmetryPattern import SymmetryPattern
