ea4945525140c0311f41851c6f06954c09a8a7ce
ElineVG <38682338+ElineVG@users.noreply.github.com>
2021-02-02 11:14:20 +0100

    Gradient in bordercolor - tryout in GradientEllipse

--- octa/shapes/GradientEllipse.py
+++ octa/shapes/GradientEllipse.py
@@ -119,6 +119,28 @@ class GradientEllipse:
             
         return gradient.get_paint_server()  
 
+    def create_bordercolor(self, dwg):
+        gradient = ""
+        if self.bordercolor[0] == "radial":
+            gradient = dwg.radialGradient()
+        elif self.bordercolor[0] == "horizontal":
+            gradient = dwg.linearGradient((0, 0), (1, 0))
+        elif self.bordercolor[0] == "vertical":
+            gradient = dwg.linearGradient((0, 0), (0, 1))
+        elif self.bordercolor[0] == "diagonal":
+            gradient = dwg.linearGradient((0, 0), (1, 1))
+        else:
+            return self.fillcolor
+            
+        dwg.defs.add(gradient)
+        # define the gradient colors
+        n_colors = len(self.bordercolor)-1
+        stepsize = 1 / (n_colors - 1)
+        for i in range(n_colors):
+            gradient.add_stop_color(i*stepsize, self.bordercolor[i+1])
+            
+        return gradient.get_paint_server()  
+    
     def generate(self, dwg):
         mirror_transform = self.create_mirror_transform()
 
@@ -128,7 +150,7 @@ class GradientEllipse:
                 center       = self.position,
                 r            = (self.bounding_box[0]/2, self.bounding_box[1]/2),
                 fill         = self.create_fillcolor(dwg),
-                stroke       = self.bordercolor,
+                stroke       = self.create_bordercolor(dwg),
                 stroke_width = self.borderwidth,
                 transform    = " ".join([mirror_transform, rotation_transform]))
         
--- octa_toolbox_demo.py
+++ octa_toolbox_demo.py
@@ -557,7 +557,7 @@ stimulus.SaveSVG("testscalingimage")
 
 #%%
 
-# gradient color
+# gradient fillcolor
 from octa.shapes import GradientEllipse
 
 stimulus = Grid(5,5, background_color = "lightgrey", row_spacing = 60, col_spacing = 60)
@@ -568,3 +568,19 @@ stimulus.shapes = GridPattern.RepeatAcrossRows([GradientEllipse])
 stimulus.fillcolors = GridPattern.RepeatAcrossRows([["radial", "white", "red"], "green", ["horizontal", "red", "orange", "green", "blue", "indigo", "violet"], ["vertical", "green", "white", "green"], ["diagonal", "red", "white"]])
 stimulus.Show()
 stimulus.SaveSVG("testgradientellipse")
+
+#%%
+
+# gradient bordercolor
+from octa.shapes import GradientEllipse
+
+stimulus = Grid(5,5, background_color = "lightgrey", row_spacing = 60, col_spacing = 60)
+stimulus._autosize_method = "maximum_bounding_box"
+
+      
+stimulus.shapes = GridPattern.RepeatAcrossRows([GradientEllipse])
+stimulus.bounding_boxes = GridPattern.RepeatAcrossRows([(40,40)])
+stimulus.borderwidths = GridPattern.RepeatAcrossRows([10,10,0,10,10])
+stimulus.bordercolors = GridPattern.RepeatAcrossRows([["radial", "white", "red"], ["horizontal", "red", "orange", "green", "blue", "indigo", "violet"], "green", ["vertical", "green", "white", "green"], ["diagonal", "red", "white"]])
+stimulus.Show()
+stimulus.SaveSVG("testgradientellipseborder")
